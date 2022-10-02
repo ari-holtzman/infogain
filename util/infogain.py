be64fc32d5deb2511ab5c7d599d2f49ef701971e
Ari <20871523+ari-holtzman@users.noreply.github.com>
2022-10-01 21:49:43 -0700

    only remove values now keys

--- util/infogain.py
+++ util/infogain.py
@@ -2,9 +2,12 @@ import random
 
 def e2e_gen_tests(mr, n_shuff_pos=0):
     mr = mr.split(', ')
-    poss = [ ', '.join(mr) ] + [ ', '.join(random.sample(mr, k=len(mr))) for _ in n_shuff_pos ]
+    poss = [ ', '.join(mr) ] + [ ', '.join(random.sample(mr, k=len(mr))) for _ in range(n_shuff_pos) ]
     negs = []
     for i in range(len(mr)):
-        neg = ', '.join((mr[:i] + mr[i+1:]))
+        item = mr[i]
+        key, value = mr[i].split('[')
+        key_only = key + '[]'
+        neg = ', '.join((mr[:i] + [key_only] + mr[i+1:]))
         negs.append(neg)
     return poss, negs
