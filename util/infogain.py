1781bb7f991eba4a1b5ccc2b654a7f458f77bd5b
Ari <20871523+ari-holtzman@users.noreply.github.com>
2022-10-04 10:59:57 -0700

    add multirc utils

--- util/infogain.py
+++ util/infogain.py
@@ -1,4 +1,19 @@
 import random 
+import re
+
+def get_pad_lens(m, pad=0):
+  if len(m) < 2:
+    return [0]*len(m)
+  max_len = float('-inf')
+  for row in m:
+    max_len = max(max_len, len(row))
+  pad_lens = []
+  for row in m:
+    pad_len = max_len-len(row)
+    pad_lens.append(pad_len)
+  return pad_lens
+
+# NLG
 
 def e2e_gen_tests(mr, n_shuff_pos=0):
     mr = mr.split(', ')
@@ -11,3 +26,21 @@ def e2e_gen_tests(mr, n_shuff_pos=0):
         neg = ', '.join((mr[:i] + [key_only] + mr[i+1:]))
         negs.append(neg)
     return poss, negs
+
+# NLU
+
+def multirc_make_tests(question_text, answer_texts):
+  tests = []
+  for answer in answer_texts:
+    pos = f'{question_text.strip()} {answer.strip()}'
+    negs = [ f'{answer.strip()}', f'{question_text.strip()}' ]
+    tests.append((pos, negs))
+  return tests
+
+def multirc_proc_passage(s):
+  s = re.sub('<b>.*?</b>', '', s)
+  secs = s.split('<br>')
+  secs = [ sec.strip() for sec in secs ]
+  secs = list(filter(lambda s: len(s) > 0, secs))
+  secs = [ ' '.join(sec.split()) for sec in secs]
+  return secs
